6000824bdfaf37cff1b6f9d058c92a5f1c960a31
amefire <abdallahmefire@gmail.com>
2021-04-06 17:00:22 +0600

    app connected to mongodb with mikro-orm/ data manipulation using mikro-orm

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -2,14 +2,11 @@ import { _prod_ } from "./constants";
 import { Post } from "./entities/Post";
 import { MikroORM } from '@mikro-orm/core';
 
-//import path from "path";
+
 
 export default {
     
-    // migrations:{
-    // path: path.join(__dirname,"./migrations"), 
-    // pattern: /^[\w-]+\d+\.[tj]s$/, // regex pattern for the migration files
-    // },
+
     entities:[Post],
     dbName:'tradingdb',
     type:'mongo', 
